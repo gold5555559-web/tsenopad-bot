9141a0e38b2ddb92d2b212f6e57e7ba6e30f2e1b
gold5555559-web <gold5555559@gmail.com>
2026-04-13 02:45:44 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -76,7 +76,7 @@ def main():
     app.add_handler(CommandHandler("start", start))
     app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_message))
     logger.info("Бот запущен!")
-    app.run_polling(drop_pending_updates=True)
+   app.run_polling()
 
 if __name__ == "__main__":
     main()
